c1bcdb6962336ccc249e5eab434f96e8440acf07
Sabrina <sabrina@msn.com>
2022-09-04 00:09:21 +0400

    added partials

--- index.js
+++ index.js
@@ -3,24 +3,28 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:Lemontree1@localhost:5432/local_characters',
+  ssl: process.env.DATABASE_URL ? true : false
+})
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
+  .get('/', async (req, res) => {
     try {
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM characters');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
+      res.render('pages/index', results );
       client.release();
     } catch (err) {
       console.error(err);
